eff99d74792beafa0b8b7cf1502a0892e7a167b4
Jaa <jaava.script@gmail.com>
2017-10-25 15:59:56 +0700

    Update Chapter 4
    - edit chapter 4 Python 101.docx
    - edit example code

--- python101/lists.py
+++ python101/lists.py
@@ -1,7 +1,10 @@
 numbers = [1, 4, 9, 16, 25]
 names = ['Alice', 'John', 'Dave', 'Lisa', 'Matt']
+mixed = [5, -7, 47.5, 'Matt']
 print(numbers)
 print(names)
+print(mixed)
 
 print(numbers[1:-1])
 print(names[1:4])
+print(mixed[:0])
